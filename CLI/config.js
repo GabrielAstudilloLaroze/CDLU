02f2e491d54c7c21361eb50b7bbd40c6af593314
GabrielAstudilloLaroze <100246658+GabrielAstudilloLaroze@users.noreply.github.com>
2022-02-23 10:31:26 -0300

    Update config.js

--- CLI/config.js
+++ CLI/config.js
@@ -2,7 +2,7 @@ module.exports = {
   saveLocation: './gz',
   unpackLocation: './datos', // 
   apiUrl: 'https://api.inshosteddata.com/api',
-  key: 'b19de8221ad8782211bef433b9f25e0c9ea61afc',
-  secret: '3af306fe9d2071a7f85a4f832427f8c29eabd3a9',
+  key: 'api_key',
+  secret: 'api_secret',
   maxConnections: 200,
 }
