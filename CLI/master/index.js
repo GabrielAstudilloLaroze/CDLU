1e1feff7a56f3c29942baa6f9b364398a196dd4b
GabrielAstudilloLaroze <100246658+GabrielAstudilloLaroze@users.noreply.github.com>
2022-02-23 14:34:13 -0300

    Delete CLI directory

--- CLI/master/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  Api: require('./lib/Api'),
-  Sync: require('./lib/Sync'),
-  cli: require('./lib/cli'),
-  Fetch: require('./lib/Fetch'),
-  Unpack: require('./lib/Unpack')
-}
--- CLI/master/test/fixtures/mockLogger.js
+++
@@ -1,3 +0,0 @@
-var logger = require('../../src/logger')
-logger.logIt = function() {}
-module.exports = logger
