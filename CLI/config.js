2d174f5b64e39c5c9670e5e797818f6a2e6923e4
GabrielAstudilloLaroze <100246658+GabrielAstudilloLaroze@users.noreply.github.com>
2022-02-23 10:34:01 -0300

    Delete config.js

--- CLI/config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  saveLocation: './gz',
-  unpackLocation: './datos', // 
-  apiUrl: 'https://api.inshosteddata.com/api',
-  key: 'api_key',
-  secret: 'api_secret',
-  maxConnections: 200,
-}
